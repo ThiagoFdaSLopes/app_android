7fa69094242224a33d7c7a27b8c77e72eb1608e8
Thiago Lopes <programadorthiagolopes@gmail.com>
2025-03-04 10:49:23 -0300

    feat: Tela de registro adicionado a MainActivity.kt para teste

--- app/src/main/java/com/grupo/appandroid/MainActivity.kt
+++ app/src/main/java/com/grupo/appandroid/MainActivity.kt
@@ -4,16 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
-import com.grupo.appandroid.ui.theme.AppAndroidTheme
+import com.grupo.appandroid.ui.theme.RegistrationAppTheme
+import com.grupo.appandroid.views.RegistrationScreen
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -29,30 +23,9 @@ class MainActivity : ComponentActivity() {
         }
         enableEdgeToEdge()
         setContent {
-            AppAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            RegistrationAppTheme {
+                RegistrationScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppAndroidTheme {
-        Greeting("Android")
     }
 }
